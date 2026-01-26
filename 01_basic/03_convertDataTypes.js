478a929aa4a17e73dffdb8d1c66e95984e867c7e
Ajit-maker413 <25mcmc39@uohyd.ac.in>
2026-01-26 11:49:00 +0000

    convert_dataTypes

---
+++ 01_basic/03_convertDataTypes.js
@@ -0,0 +1,10 @@
+let score=false
+console.log(typeof score);
+console.log(typeof (score));
+
+//let valueInNumber= Number(score)
+console.log(typeof score);
+//console.log(valueInNumber);
+let someNumber=33
+let valueInNumber=String(someNumber)
+console.log(typeof valueInNumber);
