f724dbd7f2f9e7be738a295942f76c760ebdf84c
Ajit-maker413 <25mcmc39@uohyd.ac.in>
2026-01-26 11:34:02 +0000

    variable

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,10 @@
+const accountID=12345
+let accouuntEmail="ujjwalt768@gamilcom"
+var accountPassword="Ujjwal@123"
+let accountState;
+// changes
+
+//accountID=123
+accouuntEmail="asdfghjk"
+accountPassword="asdfghjty"
+console.table([accountID,accountPassword,accouuntEmail,accountState]);
