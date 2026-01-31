f607949b08fcae4a7b1d62b393bec08af60998fc
Ajit-maker413 <25mcmc39@uohyd.ac.in>
2026-01-31 05:22:55 +0000

    Maths_function

---
+++ 01_basic/04_string.js
@@ -0,0 +1,18 @@
+const name="ajit"
+const repoCount=50
+console.log(name+repoCount)
+console.log(`hello my name is ${name} and may repo Count is ${repoCount}`)
+const gameName=new String("Ajit")
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName[1]);
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf('T'));
+console.log(gameName.substring(2,3));
+const newString="    Ajit"
+console.log(gameName);
+console.log(gameName.trim());
+const url="https://:ajit.com/ajit%20tiwari"
+console.log(url.replace('%20','%40'));
+ const gamename="Ajit_Tiwari_from_up"
+ console.log(gamename.split('_'));
